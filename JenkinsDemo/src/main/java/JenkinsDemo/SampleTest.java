b8399886531d35366b81eaeef780dab718199302
dirkishore <kishorekishore2610@gmail.com>
2022-11-27 09:54:44 +0530

    Initial commit

---
+++ JenkinsDemo/src/main/java/JenkinsDemo/SampleTest.java
@@ -0,0 +1,23 @@
+package JenkinsDemo;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class SampleTest {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.edge.driver",
+				"C://Users//kishore//Downloads//edgedriver_win64//msedgedriver.exe");
+
+		WebDriver driver = new EdgeDriver();
+
+		driver.get("https://www.google.com/");
+
+		Thread.sleep(3000);
+
+		driver.quit();
+	}
+
+}
